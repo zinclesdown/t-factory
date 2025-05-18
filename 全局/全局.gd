43d65e96a2dd1ea46a39fa6a308e20bfb6640e23
zincles <zinclesdown@gmail.com>
2025-05-19 04:45:30 +0800

    准备编写物流系统

--- 全局/全局.gd
+++ 全局/全局.gd
@@ -1,3 +1,5 @@
 class_name 全局
 
 static var 重力加速度 := Vector2(0, 40)
+
+const 全局字体 := preload("res://资源/字体/MapleMono-SC-NF-Regular.ttf")
--- 测试/2D特殊工厂物流测试/2d工厂物流测试.gd
+++ 测试/2D特殊工厂物流测试/2d工厂物流测试.gd
@@ -1,35 +1,47 @@
 extends Node2D
 ## 2D工厂物流系统.
 
-## 1. 定义节点   Node
+## 1. 定义工厂   Producer
 ## 2. 定义连接器 Connector
 ## 3. 进行寻路   Path Finder
 ## 4. 进行帧Tick Tick
 
 ## NOTE 最后记得把IMGUI进行特殊化.
 
-var _IMGUI_SHOW_DEMO_WINDOW := [false]
-var _IMGUI_SHOW_LOGISTIC_DEBUG_WINDOW := [true]
+var _IMGUI_显示DEMO面板 := [false]
+var _IMGUI_显示调试面板 := [true]
 
-func _ready() -> void:
-	ImGuiGD.Connect(_IMGUI)
 
-func _IMGUI()->void:
+## 有 "连接器"  "生产器" 两种节点.
+##
+
+class 生产器:
+	pass
+
+class 连接器:
+	pass
+
+
+func _process(delta: float) -> void:
+	_IMGUI()
+	queue_redraw()
+
+func _IMGUI() -> void:
 	# 主菜单
 	if ImGui.BeginMainMenuBar():
 		if ImGui.BeginMenu("调试"):
 			if ImGui.MenuItem("打卡物流面板"):
-				_IMGUI_SHOW_LOGISTIC_DEBUG_WINDOW[0] = !_IMGUI_SHOW_LOGISTIC_DEBUG_WINDOW[0]
+				_IMGUI_显示调试面板[0] = !_IMGUI_显示调试面板[0]
 			ImGui.EndMenu()
 		if ImGui.BeginMenu("帮助"):
 			if ImGui.MenuItem("打开Demo"):
-				_IMGUI_SHOW_DEMO_WINDOW[0] = !_IMGUI_SHOW_DEMO_WINDOW[0]
+				_IMGUI_显示DEMO面板[0] = !_IMGUI_显示DEMO面板[0]
 			ImGui.EndMenu()
 		ImGui.EndMainMenuBar()
 
 	# 物流系统面板
-	if _IMGUI_SHOW_LOGISTIC_DEBUG_WINDOW[0]:
-		ImGui.Begin("物流系统测试", _IMGUI_SHOW_LOGISTIC_DEBUG_WINDOW, ImGui.WindowFlags_MenuBar)
+	if _IMGUI_显示调试面板[0]:
+		ImGui.Begin("物流系统测试", _IMGUI_显示调试面板, ImGui.WindowFlags_MenuBar)
 		
 		if ImGui.BeginTable("Table", 3):
 			ImGui.TableNextRow()
@@ -41,17 +53,51 @@ func _IMGUI()->void:
 		ImGui.End()
 		
 	# 可选的 Demo 窗口
-	if _IMGUI_SHOW_DEMO_WINDOW[0]:
-		ImGui.ShowDemoWindow(_IMGUI_SHOW_DEMO_WINDOW)
+	if _IMGUI_显示DEMO面板[0]:
+		ImGui.ShowDemoWindow(_IMGUI_显示DEMO面板)
 
-func 添加节点()->void:
+func 添加生产器(pos:Vector2, 生产器类型:Enum生产器类型)->void:
+	print("试图在%s放置%s了!" % [str(pos), str(生产器类型)])
+
+func 添加连接器(pos:Vector2, connector:连接器)->void:
 	pass
 
-func 添加连接器()->void:
-	pass
+#func 更新寻路()->void:
+	#pass
 
-func 更新寻路()->void:
-	pass
+const 网格大小 := Vector2(32,32)
 
 func Tick()->void:
 	pass
+
+var 当前物品 := Enum物品.生产器
+
+enum Enum物品 {
+	生产器,
+	连接器
+}
+
+enum Enum生产器类型 {
+	风力发电机,
+}
+
+func 获取吸附网格中心后的点(pos:Vector2) -> Vector2:
+	var 原点位置 :Vector2= floor(pos / 网格大小) * 网格大小
+	return 原点位置 + 网格大小/2
+
+func _draw() -> void:
+	# 绘制红色框框
+	draw_rect(Rect2(获取吸附网格中心后的点(get_global_mouse_position())-网格大小/2.0, 网格大小), Color.RED, false)
+	draw_string(全局.全局字体, get_global_mouse_position(), str(get_global_mouse_position()))
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		var 吸附后鼠标位置 := 获取吸附网格中心后的点(get_global_mouse_position())
+		
+		if 当前物品 == Enum物品.生产器:
+			添加生产器(吸附后鼠标位置, Enum生产器类型.风力发电机)
+
+
+func _ready() -> void:
+	pass
+	
